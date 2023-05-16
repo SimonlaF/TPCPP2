39b5063f309c93f44254124a64d4c35376e1a6dd
Simon et Fanir <simonferrier@icloud.com>
2023-05-17 00:04:41 +0200

    Début TEST unitaire

---
+++ TestParseur/TestParseur.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Simon\Documents\Cours\C++\TPCPP2\PROJETCPP2\Cercle.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestParseur
+{
+	TEST_CLASS(TestParseur)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::string parse;
+
+		}
+	};
+}
